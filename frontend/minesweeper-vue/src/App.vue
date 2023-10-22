a71ae641771937cdf6e62117372c6b39bc673898
Leoyna <Leoynaaa2@gmail.com>
2023-10-22 13:58:11 +0200

    Commit

--- frontend/minesweeper-vue/src/App.vue
+++ frontend/minesweeper-vue/src/App.vue
@@ -1,20 +1,40 @@
 <template>
+  
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="App">
+    <form @submit.prevent="submitUsername">
+      <input type="text" placeholder="Enter username" v-model="message" />
+      <button type="submit">Submit</button>
+    </form>
+    <div class="box">
+      <div class="messages"></div>
+      <form class="input-div" @submit.prevent="submitMessage">
+        <input type="text" placeholder="Type in text" v-model="inputMessageText" />
+        <button type="submit">Submit</button>
+      </form>
+  </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import SocketioService from './services/socketio.service.js';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   },
+
+  /*
   created() {
     SocketioService.setupSocketConnection();
+  },*/
+
+  submitUsername() {
+      console.log(this.username);
+      SocketioService.setupSocketConnection(this.username);
   },
+  
   beforeUnmount() {
     SocketioService.disconnect();
   }
