29f8677a96b1a3d13083095817259e9017e5585b
Leoyna <Leoynaaa2@gmail.com>
2023-10-22 12:22:43 +0200

    Part 1 Done!

--- frontend/minesweeper-vue/src/services/socketio.service.js
+++ frontend/minesweeper-vue/src/services/socketio.service.js
@@ -6,6 +6,7 @@ class SocketioService {
   
     setupSocketConnection() {
       this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT);
+      this.socket.emit('my message', 'Hello there from Vue.');
       this.socket.on('my broadcast', (data) => {
         console.log(data);
       });
