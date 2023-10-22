be7a9e62c91d42c1d002659d52588a0908e6f362
Leoyna <Leoynaaa2@gmail.com>
2023-10-22 14:05:13 +0200

    css

--- frontend/minesweeper-vue/src/main.js
+++ frontend/minesweeper-vue/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import "/Users/Leoyna1/Documents/GitHub/ProjectMinesweeper/frontend/minesweeper-vue/public/css/App.css"
 
 createApp(App).mount('#app')
