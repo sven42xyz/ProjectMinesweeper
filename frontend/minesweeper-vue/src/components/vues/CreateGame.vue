fc2371e8d798b74a78a243605980d7adef7ecc75
Leoyna <Leoynaaa2@gmail.com>
2023-10-28 22:26:32 +0200

    Changed Create Game

--- frontend/minesweeper-vue/src/components/vues/CreateGame.vue
+++ frontend/minesweeper-vue/src/components/vues/CreateGame.vue
@@ -1,57 +1,47 @@
 <template>
-  <div class="d-flex justify-content-md-center">
-    <div class="card text-bg-primary" style="width: 36rem;">
-      <div class="card-body">
-        <h1 class="card-header mb-4">Create Game</h1>
-        <form v-on:submit.prevent>
-          <div class="container">
-            <div class="d-flex justify-content-center">
-              <div class="row">
-                <div class="col-12">
-                  <div class="input-group md-4 w-100 mb-3">
-                    <span class="input-group-text" id="basic-addon1">room</span>
-                    <input type="text" class="form-control" aria-label="Roomcode" v-model="room">
-                  </div>
+  <div class="container-fluid medium-fluid-container">
+    <h1 class="card-header mt-2 mb-2" style="font-size: 6vh;">Create Game</h1>
+    <hr style="margin-left: 2vw; margin-right: 2vw;"/>
+    <form v-on:submit.prevent class="create-game-form">
+      <div class="row row-cols-1">
+        <div class="col mb-2" style="padding-right: 0%;">
+          <div class="input-group md-4 mb-3" style="height: 5.5vh; width: 100%;">
+            <span class="input-group-text" id="basic-addon1" style="font-size: 2vh; ">room</span>
+            <input type="text" class="form-control" aria-label="Roomcode" v-model="room"  style="height: 5.5vh;">
+          </div>
+        </div>
+        <div class="col">
+          <div class="d-flex justify-content-md-left mb-3">
+            <form v-on:submit.prevent>
+              <div class="form-group">
+                <h5 style="font-size: 2.5vh;">Choose difficulty:</h5>
+              </div>
+              <div class="form-group">
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="dif" id="dif-1" value="dif-1" checked>
+                  <label class="form-check-label" for="dif-1">Easy</label>
                 </div>
-                <div class="col-12">
-                  <div class="d-flex justify-content-md-left mb-3">
-                    <form v-on:submit.prevent>
-                      <div class="form-group">
-                        <h5>Choose difficulty:</h5>
-                      </div>
-                      <div class="form-group">
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="dif" id="dif-1" value="dif-1" checked>
-                          <label class="form-check-label" for="dif-1">Easy</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="dif" id="dif-2" value="dif-2">
-                          <label class="form-check-label" for="dif-2">Medium</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="dif" id="dif-3" value="dif-3">
-                          <label class="form-check-label" for="dif-3">Hard</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="dif" id="dif-4" value="dif-4">
-                          <label class="form-check-label" for="dif-4">Insane</label>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="dif" id="dif-2" value="dif-2">
+                  <label class="form-check-label" for="dif-2">Medium</label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="dif" id="dif-3" value="dif-3">
+                  <label class="form-check-label" for="dif-3">Hard</label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="dif" id="dif-4" value="dif-4">
+                  <label class="form-check-label" for="dif-4">Insane</label>
                 </div>
               </div>
-            </div>
+            </form>
           </div>
-          <div class="d-flex justify-content-center">
-            <div class="text-center w-25">
-              <button class="btn btn-danger mb-1" type="Submit" id="Submit-Button" aria-expanded="false">Create
-                Game</button>
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
-    </div>
+      <hr class="m-0"/>
+      <button class="btn btn-success" type="Submit" id="Submit-Button" aria-expanded="false">Create Game</button>
+      <button class="btn btn-danger" type="Cancel" id="Cancel-Button" aria-expanded="false">Cancel</button>
+    </form>
   </div>
 </template>
 
@@ -86,5 +76,30 @@ export default {
 .form-check-label {
   text-align: left;
   display: block;
+  padding-top: 0.25vh;
+  font-size: 2vh;
+}
+
+.btn-success{
+  margin-right: 10.5vw;
+}
+.btn{
+  margin-top: 2vw;
+}
+
+.form-check-input{
+  height: 2.25vh;
+  width: 2.25vh;
+}
+.form-check{
+  min-height: 2.25vh;
+  min-width: 2.25vh;
+}
+
+.create-game-form{
+  padding: 5%;
+  padding-top: 1%;
+  width: 40vw;
+
 }
 </style>
--- frontend/minesweeper-vue/src/components/vues/LandingPage.vue
+++ frontend/minesweeper-vue/src/components/vues/LandingPage.vue
@@ -1,31 +1,3 @@
-<!-- <template>
-  <div class="d-flex justify-content-md-center">
-    <div class="card text-bg-primary" style="width: 36rem;">
-      <div class="card-body">
-        <h1 class="card-header mb-4">Minesweeper.io</h1>
-        <form v-on:submit.prevent>
-          <div class="d-flex justify-content-center">
-            <div class="input-group md-4 w-75 mb-3">
-              <span class="input-group-text" id="basic-addon1">username</span>
-              <input type="text" class="form-control" aria-label="Username" v-model="username">
-            </div>
-          </div>
-          <div class="d-flex justify-content-center">
-            <div class="text-center w-25">
-              <button v-on:click="newGame" class="btn btn-danger mb-1" type="Submit" id="new-game"
-                aria-expanded="false">New
-                Game</button>
-              <button v-on:click="joinGame" class="btn btn-warning mt-1" type="Submit" id="join-game"
-                aria-expanded="false">Join
-                Game</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</template> -->
-
 <template>
   <div id = "WelcomeDiv">
     <div class="container-fluid small-fluid-container">
