6ee2b860308d9ef0d515276aff5e3e3686bcda0d
Leoyna <Leoynaaa2@gmail.com>
2023-10-30 19:15:39 +0100

    Disabled the radio buttons

--- frontend/minesweeper-vue/src/components/scraps/CurrentlySelectedDifficulty.vue
+++ frontend/minesweeper-vue/src/components/scraps/CurrentlySelectedDifficulty.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="form-group">
         <div class="form-check">
-        <input class="form-check-input" type="radio" name="dif" id="dif-1" value="dif-1" checked v-model="difficulty">
+        <input class="form-check-input" tabindex="-1" type="radio" name="dif" id="dif-1" value="dif-1" checked v-model="difficulty">
         <label class="form-check-label" for="dif-1">Easy</label>
         </div>
         <div class="form-check">
-        <input class="form-check-input" type="radio" name="dif" id="dif-2" value="dif-2" v-model="difficulty">
+        <input class="form-check-input" tabindex="-1" type="radio" name="dif" id="dif-2" value="dif-2" v-model="difficulty">
         <label class="form-check-label" for="dif-2">Medium</label>
         </div>
         <div class="form-check">
-        <input class="form-check-input" type="radio" name="dif" id="dif-3" value="dif-3" v-model="difficulty">
+        <input class="form-check-input" tabindex="-1" type="radio" name="dif" id="dif-3" value="dif-3" v-model="difficulty">
         <label class="form-check-label" for="dif-3">Hard</label>
         </div>
         <div class="form-check">
-        <input class="form-check-input" type="radio" name="dif" id="dif-4" value="dif-4" v-model="difficulty">
+        <input class="form-check-input" tabindex="-1" type="radio" name="dif" id="dif-4" value="dif-4" v-model="difficulty">
         <label class="form-check-label" for="dif-4">Insane</label>
         </div>
     </div>
@@ -30,4 +30,7 @@
       padding-left: 1vw;
       padding-right: 6vw;
       }
+    .form-check-input{
+        pointer-events: none;
+    }
 </style>
