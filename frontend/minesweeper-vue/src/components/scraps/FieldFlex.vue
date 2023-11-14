25b686e276373236a085e3394d7cab3f1d7efd8b
Leoyna <Leoynaaa2@gmail.com>
2023-11-14 15:56:50 +0100

    Buttons kennen Ihre Nachbarn

--- frontend/minesweeper-vue/src/components/scraps/FieldFlex.vue
+++ frontend/minesweeper-vue/src/components/scraps/FieldFlex.vue
@@ -29,19 +29,57 @@
         },
         methods:{
             clicked(row, col){
-                console.log(this.gameboard[row-1]);
-                console.log(this.$refs);
                 this.gameboard[row-1][col-1].setIsRevealed();
+                console.log(row, col);
+                console.log(this.gameboard)
+                console.log(this.gameboard[row-1])
+                console.log(this.gameboard[col-1])
+                var w = row - 1;
+                var h = col - 1;
                 for(const [key, value] of Object.entries(this.$refs)){
-                    if(key == ("["+ row + "," + col + "]")){
+                    var check = "["+ row + "," + col + "]";
+                    console.log(check);
+                    if(key == check){
                         value[0].color = 'grey';
                         value[0].enabled = 'none';
 
                         if(this.gameboard[row-1][col-1].IsBomb){
                             value[0].isBomb = 'X';
+                        }else{
+                            var number = this.revealNeighbours(w, h);
+                            if(number != 0){
+                            value[0].isNumber = number;
+                            console.log(number);}
                         }
                     }
                 }
+            },
+            revealNeighbours(row, col){
+                var neighbouringBombs = 0;
+                console.log(row,col)
+/*                 const refEntries = Object.entries(this.$refs);
+                for(var i = 0; i < 3; i++){
+                    for(var j= 0; j < 3; j++){
+                        var x = (row - 1 + i);
+                        var y = (col - 1 + j);
+                        if(x > 0 && y > 0 && x < this.size && y < this.size && this.gameboard[x][y].IsBomb){
+                            neighbouringBombs = neighbouringBombs + 1;
+                            console.log(this.gameboard[x][y]);
+                        }else{
+                            var cur = "["+ (x + 1) + "," + (y + 1) + "]";
+                            var thisEntry = refEntries.find(i => i[0] === cur);
+                            console.log(thisEntry);
+                            if(thisEntry && thisEntry[1][0].color != 'grey' && this.gameboard[x][y].IsBomb == false){
+                                thisEntry[1][0].color = 'grey';
+                                thisEntry[1][0].enabled = 'none';
+                                //this.gameboard[x][y].nBomb = this.revealNeighbours(x, y);
+                                console.log(thisEntry[1][0].isNumber);
+                            }
+                        }
+                        
+                    }
+                } */
+                return neighbouringBombs;
             },
             ref(i, x) {
                 return("["+ i + "," + x + "]");
@@ -53,7 +91,7 @@
     };
     function createBoard(row, col){
         var a = []
-        var bombs = (row + col) * 0.75;
+        var bombs = (row + col)* 2;
         for(var i = 0; i < row; i++){
             a[i] = []
             for(var j=0; j < col; j++){
@@ -69,11 +107,26 @@
                 w = Math.floor(Math.random()* col);
                 h = Math.floor(Math.random() * row);
             }
-            console.log(a[w][h]);
             a[w][h].IsBomb = true; //make new position is a bomb
         }
+        for(i = 0; i < row; i++){
+            for( j=0; j < col; j++){
+                var neighbouringBombs = 0;
+                for(var k = 0; k < 3; k++){
+                    for(var t= 0; t < 3; t++){
+                        var x = (i - 1 + k);
+                        var y = (j - 1 + t);
+                        if(x >= 0 && y >= 0 && x < row && y < row && a[x][y].IsBomb == true){
+                            neighbouringBombs ++;
+                        }                        
+                    }
+                }
+                a[i][j].nBombs = neighbouringBombs;
+            }
+        }
+        console.log(a);
         return a
-    } 
+    }
 </script>
   
 <style scoped>
